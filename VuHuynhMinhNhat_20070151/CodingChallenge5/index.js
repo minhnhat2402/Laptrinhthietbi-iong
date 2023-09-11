bb5bd004a769b415b3a894d1d8e0d24a78f063f1
minhnhat2402 <125148419+minhnhat2402@users.noreply.github.com>
2023-09-12 01:29:23 +0700

    Add files via upload

--- VuHuynhMinhNhat_20070151/CodingChallenge5/index.js
+++ VuHuynhMinhNhat_20070151/CodingChallenge5/index.js
@@ -1,5 +1,17 @@
-var dolphinsScores = [44, 23, 71]
-var koalasScores = [65, 54, 49]
+var calcAverage = function (score1, score2, score3) {
+    return (score1 + score2 + score3) / 3;
+};
 
+var avgDolphins = calcAverage(41, 23, 71);
+var avgKoalas = calcAverage(65, 54, 49);
 
-var dolphinsAverageScore = function calcAverage(dolphinsScores.reduce((acc, num) => { return acc + num })) / 3;
+var checkWinner = function (avgDolphins, avgKoalas) {
+    if (avgDolphins >= 2 * avgKoalas) {
+        console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+    } else if (avgKoalas >= 2 * avgDolphins) {
+        console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+    } else {
+        console.log("No team wins!");
+    }
+};
+checkWinner(avgDolphins, avgKoalas);
