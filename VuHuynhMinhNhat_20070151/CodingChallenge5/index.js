d21441ab0d945e5106ab29e51d22cc0c29f90c81
minhnhat2402 <125148419+minhnhat2402@users.noreply.github.com>
2023-09-12 00:37:29 +0700

    Add files via upload

---
+++ VuHuynhMinhNhat_20070151/CodingChallenge5/index.js
@@ -0,0 +1,5 @@
+var dolphinsScores = [44, 23, 71]
+var koalasScores = [65, 54, 49]
+
+
+var dolphinsAverageScore = function calcAverage(dolphinsScores.reduce((acc, num) => { return acc + num })) / 3;
